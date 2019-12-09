b57a2b86321fb9acfe2288b77d3f05bd51b85fa6
Archie Espelimbergo Cortez <fdeu.aalmine@gmail.com>
2019-12-09 13:15:49 +0800

    site prep

--- plotapp/models.py
+++ plotapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class InputFile(models.Model):
+    file = models.FileField(_("Input File"))
+    description = models.CharField(_("Description"), max_length=255, null=True, blank=True)
+    dete_uploaded = models.DateTimeField(_("Date Uploaded"), auto_now_add=True)
