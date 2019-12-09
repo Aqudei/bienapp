d523343604ae40f6d69e87023f14edd65361c1bb
Archie Espelimbergo Cortez <fdeu.aalmine@gmail.com>
2019-12-09 13:19:24 +0800

    site prep

--- plotapp/models.py
+++ plotapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils.text import gettext_lazy as _
 
 # Create your models here.
 
+
 class InputFile(models.Model):
     file = models.FileField(_("Input File"))
-    description = models.CharField(_("Description"), max_length=255, null=True, blank=True)
+    description = models.CharField(
+        _("Description"), max_length=255, null=True, blank=True)
     dete_uploaded = models.DateTimeField(_("Date Uploaded"), auto_now_add=True)
